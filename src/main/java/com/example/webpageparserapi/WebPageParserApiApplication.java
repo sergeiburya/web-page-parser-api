dfdbb1aa4f9f5d06daa92ef162db13480e7c899d
Serhii Buria <sergeiburya@gmail.com>
2023-01-05 10:52:08 +0200

    initial progect

---
+++ src/main/java/com/example/webpageparserapi/WebPageParserApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.webpageparserapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPageParserApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebPageParserApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webpageparserapi/WebPageParserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webpageparserapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPageParserApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
